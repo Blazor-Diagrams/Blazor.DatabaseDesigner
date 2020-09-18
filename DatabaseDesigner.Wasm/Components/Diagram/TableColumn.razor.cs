7a443445b77d5c493c663b93c3c7b2721a37d9dd
zHaytam <haytam.zanid@gmail.com>
2020-09-18 18:56:49 +0100

    Change layout (add sidebar)

--- DatabaseDesigner.Wasm/Components/Diagram/TableColumn.razor.cs
+++ DatabaseDesigner.Wasm/Components/Diagram/TableColumn.razor.cs
@@ -1,11 +1,24 @@
 ﻿using Microsoft.AspNetCore.Components;
 using DatabaseDesigner.Core.Models;
+using System.Threading.Tasks;
+using System;
+using Microsoft.Extensions.Logging;
 
 namespace DatabaseDesigner.Wasm.Components.Diagram
 {
     public partial class TableColumn
     {
+        [Inject]
+        public ILogger Logger { get; set; }
+
         [Parameter]
         public Column Data { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+
+            Logger.LogDebug("OnAfterRenderAsync");
+        }
     }
 }
---
+++ DatabaseDesigner.Wasm/Components/Sidebar.razor.cs
@@ -0,0 +1,33 @@
+﻿using Blazor.Diagrams.Core;
+using Blazor.Diagrams.Core.Models.Base;
+using DatabaseDesigner.Core.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+
+namespace DatabaseDesigner.Wasm.Components
+{
+    public partial class Sidebar : IDisposable
+    {
+        private TableModel _selectedTable;
+
+        [CascadingParameter(Name = "DiagramManager")]
+        public DiagramManager Diagram { get; set; }
+
+        public void Dispose()
+        {
+            Diagram.SelectionChanged -= Diagram_SelectionChanged;
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            Diagram.SelectionChanged += Diagram_SelectionChanged;
+        }
+
+        private void Diagram_SelectionChanged(SelectableModel model, bool selected)
+        {
+            StateHasChanged();
+        }
+    }
+}
